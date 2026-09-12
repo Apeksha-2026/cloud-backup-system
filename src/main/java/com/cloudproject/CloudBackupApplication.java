635d7a98baf80ebfb28f7c3c625ccc8372fa0149
Harshita-Rane <harshitarane202@gmail.com>
2026-09-12 20:50:06 +0530

    pom.xml defined

---
+++ src/main/java/com/cloudproject/CloudBackupApplication.java
@@ -0,0 +1,12 @@
+package com.cloudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBackupApplication.class, args);
+    }
+}
